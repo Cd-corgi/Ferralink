e250a78c3b0995cfecefab31ac01c3f67f98b30c
Vaibhav Singh <112477955+vkamsingh@users.noreply.github.com>
2023-02-25 22:02:00 +0530

    Update FerraLink.js

--- src/FerraLink.js
+++ src/FerraLink.js
@@ -31,7 +31,6 @@ class FerraLink extends EventEmitter {
 		}
 		this.shoukaku = new Shoukaku(connector, options.nodes, options.shoukakuoptions);
 		this.players = new Map();
-		this.sendWs = options?.send || null;
 		this.defaultSearchEngine = options?.defaultSearchEngine || 'youtube';
 	}
 
